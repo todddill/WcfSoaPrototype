8e1b87d94da537a438b82fc858fd5492f7110854
Todd Dill <todd@tetranetic.com>
2013-10-09 17:03:49 -0500

    Completed SOAP integration test with HCPCS web service

--- HcpcsCodes/Communicator.cs
+++ HcpcsCodes/Communicator.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using SoaHubCore.BaseClasses;
 using SoaHubCore.Interfaces;
 
@@ -7,23 +10,66 @@ namespace HcpcsCodes
     public class Communicator : ICommunicator
     {
         private TransactionBase _transaction;
+        const string SOAPENVELOP = "xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.restfulwebservices.net/ServiceContracts/2008/01\"";
 
         public Communicator(TransactionBase transaction)
         {
             _transaction = transaction;
-            BuildRequestMessage();
-        }
-
-        private static string BuildRequestMessage()
-        {
-            return string.Empty;
         }
 
         #region ICommunicator Members
 
         public TransactionBase Send()
         {
+            string message = BuildSoapMessage(_transaction.DestinationMethod, _transaction.RequestParameters).ToString();
+            HttpWebRequest request = BuildHttpRequest(_transaction.DestinationMethod, _transaction.DestinationEndpoint);
+            string response = WebServiceCall(request, message);
+            _transaction.ResponseParameters["object"] = response;
             return _transaction;
+        }
+
+        private static string WebServiceCall(HttpWebRequest httpRequest, string soapRequest)
+        {
+            Stream requestStream = httpRequest.GetRequestStream();
+            //Create Stream and Complete Request             
+            StreamWriter streamWriter = new StreamWriter(requestStream, Encoding.ASCII);
+
+            streamWriter.Write(soapRequest);
+            streamWriter.Close();
+            //Get the Response    
+            HttpWebResponse wr = (HttpWebResponse)httpRequest.GetResponse();
+            StreamReader srd = new StreamReader(wr.GetResponseStream());
+            string resulXmlFromWebService = srd.ReadToEnd();
+            return resulXmlFromWebService;
+        }
+
+        private static HttpWebRequest BuildHttpRequest(string methodName, string url)
+        {
+            WebRequest webRequest = WebRequest.Create(url);
+            HttpWebRequest httpRequest = (HttpWebRequest)webRequest;
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "text/xml; charset=utf-8";
+            httpRequest.Headers.Add("SOAPAction:" + methodName);
+            httpRequest.ProtocolVersion = HttpVersion.Version11;
+            httpRequest.Credentials = CredentialCache.DefaultCredentials;
+            return httpRequest;
+        }
+
+        private static StringBuilder BuildSoapMessage(string methodName, Dictionary<string, string> methodParameters)
+        {
+            StringBuilder soapRequest = new StringBuilder();
+            soapRequest.Append("<soapenv:Envelope " + SOAPENVELOP + ">");
+            soapRequest.Append("<soapenv:Header/>");
+            soapRequest.Append("<soapenv:Body>");
+            soapRequest.Append("<ns:" + methodName + ">");
+            foreach (KeyValuePair<string, string> item in methodParameters)
+            {
+                soapRequest.Append("<ns:request>" + item.Value + "</ns:request>");
+            }
+            soapRequest.Append("</ns:" + methodName + ">");
+            soapRequest.Append("</soapenv:Body>");
+            soapRequest.Append("</soapenv:Envelope>");
+            return soapRequest;
         }
 
         #endregion
---
+++ HcpcsCodesTests/IntegrationTests/CommunicatorIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Linq;
+using HcpcsCodes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoaHubCore.BaseClasses;
+using SoaHubCore.Interfaces;
+
+namespace HcpcsCodesTests.IntegrationTests
+{
+    [TestClass]
+    public class CommunicatorIntegrationTests
+    {
+        const string PATH = "configurationData.xml";
+        const string XPATH = "/hcpcscodesconfiguration";
+
+        [TestMethod]
+        public void Communicator_Send_GetResponse()
+        {
+            XDocument doc = SetUpData.ConfigurationData.GetHcpcsConfigurationData();
+            SetUpData.ConfigurationData.SaveDataToAnXmlFile(PATH);
+
+            Transaction request = new Transaction();
+            TransactionData transactionData = new TransactionData(PATH);
+            transactionData.XpathToRequestParameters = XPATH + "/request/parameters/description";
+            transactionData.XpathToResponseParameters = XPATH + "/response/parameters/object";
+            transactionData.XpathToEndpoint = XPATH + "/request/endpoint";
+            transactionData.XpathToMethod = XPATH + "/request/method";
+            request.LoadTransactionConfiguration(transactionData);
+            ICommunicator communicator = new Communicator(request);
+
+            TransactionBase response = communicator.Send();
+
+            Assert.IsTrue(response.ResponseParameters["object"].Contains("HCPCS"));
+        }
+    }
+}
